f6f23d508612038ad4c68760e8d31dd1dc589514
cragmax <glyn.s.ellis@gmail.com>
2026-02-20 12:04:50 +0000

    chore: commit server deployment files

---
+++ deploy/start.bat
@@ -0,0 +1,22 @@
+@echo off
+:: ============================================================
+:: start.bat [minMemory] [maxMemory]
+:: Template file - tokens are substituted by Gradle on deploy.
+:: Generated copy lives in server folder - do not edit that one.
+::
+:: Can also be run manually from the server folder:
+::   start.bat          -> uses baked-in defaults
+::   start.bat 2G 8G   -> overrides memory settings
+:: ============================================================
+
+:: Change to the directory where this script lives
+cd /d "%~dp0"
+
+set MIN_MEMORY=%1
+set MAX_MEMORY=%2
+
+if "%MIN_MEMORY%"=="" set MIN_MEMORY=@SERVER_MIN_MEMORY@
+if "%MAX_MEMORY%"=="" set MAX_MEMORY=@SERVER_MAX_MEMORY@
+
+java -Xms%MIN_MEMORY% -Xmx%MAX_MEMORY% -jar @SERVER_JAR@ --nogui
+pause
